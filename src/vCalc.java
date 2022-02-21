59c53dbf5268ee831eae308dd1aea67303fc22d4
HerraVp <Vp2b2t@gmail.com>
2022-02-21 18:53:56 +0200

    Theme option

--- src/vCalc.java
+++ src/vCalc.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.function.Consumer;
 
 public class vCalc implements ActionListener {
 
     JFrame frame;
+    JComboBox<String> comboCalcType, comboTheme;
     JTextField textfield;
     JButton[] numberButtons = new JButton[10];
     JButton[] functionButtons = new JButton[9];
@@ -12,7 +14,6 @@ public class vCalc implements ActionListener {
     JButton decButton, equButton, delButton, clrButton, negButton;
     JPanel panel;
     String version = "1.0";
-
     Font myFont = new Font("Consolas", Font.BOLD, 25);
 
 
@@ -23,17 +24,15 @@ public class vCalc implements ActionListener {
 
     vCalc() {
         frame = new JFrame("vCalc || v" + version);
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420, 550);
+        frame.setSize(500, 550);
         frame.setLayout(null);
-
+        comboTheme = initCombo(new String[]{"Light", "Colored", "Dark"}, 360, 20, "Theme", themeSwitchEventConsumer);
 
         textfield = new JTextField();
         textfield.setBounds(50, 25, 300, 50);
         textfield.setFont(myFont);
         textfield.setEditable(false);
-
 
         addButton = new JButton("+");
         subButton = new JButton("-");
@@ -107,6 +106,16 @@ public class vCalc implements ActionListener {
         frame.setVisible(true);
     }
 
+    private JComboBox<String> initCombo(String[] items, int x, int y, String toolTip, Consumer consumerEvent) {
+        JComboBox<String> combo = new JComboBox<>(items);
+        combo.setBounds(x, y, 140, 25);
+        combo.setToolTipText(toolTip);
+        combo.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        combo.addItemListener(consumerEvent::accept);
+        frame.add(combo);
+
+        return combo;
+    }
 
     public static void main(String[] args) {
         vCalc calc = new vCalc();
@@ -185,12 +194,12 @@ public class vCalc implements ActionListener {
 
         if (e.getSource() == delButton) {
             String string = textfield.getText();
-            textfield.setText("");
+            textfield.setText(" ");
 
-            for (int i = 0; i < string.length() - 1; i++) {
-                textfield.setText(textfield.getText() + string.charAt(i));
+            for(int i = 0; i < string.length() - 1; i++){
+                String strnew = string.substring(0, string.length()-1);
+                textfield.setText(strnew);
             }
-
         }
 
         if (e.getSource() == negButton) {
@@ -200,4 +209,98 @@ public class vCalc implements ActionListener {
             textfield.setText(String.valueOf(temp));
         }
     }
+
+    private Consumer<ItemEvent> themeSwitchEventConsumer = event -> {
+        if (event.getStateChange() != ItemEvent.SELECTED) return;
+
+        String selectedTheme = (String) event.getItem();
+        switch (selectedTheme) {
+            case "Light" -> {
+                frame.getContentPane().setBackground(null);
+                clrButton.setBackground(null);
+                mulButton.setBackground(null);
+                divButton.setBackground(null);
+                subButton.setBackground(null);
+                addButton.setBackground(null);
+                //rootButton.setBackground(null);
+                equButton.setBackground(null);
+                numberButtons[0].setBackground(null);
+                numberButtons[1].setBackground(null);
+                numberButtons[2].setBackground(null);
+                numberButtons[3].setBackground(null);
+                numberButtons[4].setBackground(null);
+                numberButtons[5].setBackground(null);
+                numberButtons[6].setBackground(null);
+                numberButtons[7].setBackground(null);
+                numberButtons[8].setBackground(null);
+                numberButtons[9].setBackground(null);
+                decButton.setBackground(null);
+                clrButton.setForeground(Color.BLACK);
+                divButton.setForeground(Color.BLACK);
+                mulButton.setForeground(Color.BLACK);
+                subButton.setForeground(Color.BLACK);
+                addButton.setForeground(Color.BLACK);
+                equButton.setForeground(Color.BLACK);
+            }
+            //rootButton.setForeground(Color.BLACK);
+            case "Colored" -> {
+                frame.getContentPane().setBackground(null);
+                clrButton.setBackground(Color.RED);
+                divButton.setBackground(Color.PINK);
+                mulButton.setBackground(Color.PINK);
+                subButton.setBackground(Color.PINK);
+                addButton.setBackground(Color.PINK);
+                //rootButton.setBackground(Color.PINK);
+                equButton.setBackground(Color.BLUE);
+                decButton.setBackground(Color.WHITE);
+                numberButtons[0].setBackground(Color.WHITE);
+                numberButtons[1].setBackground(Color.WHITE);
+                numberButtons[2].setBackground(Color.WHITE);
+                numberButtons[3].setBackground(Color.WHITE);
+                numberButtons[4].setBackground(Color.WHITE);
+                numberButtons[5].setBackground(Color.WHITE);
+                numberButtons[6].setBackground(Color.WHITE);
+                numberButtons[7].setBackground(Color.WHITE);
+                numberButtons[8].setBackground(Color.WHITE);
+                numberButtons[9].setBackground(Color.WHITE);
+                clrButton.setForeground(Color.WHITE);
+                divButton.setForeground(Color.WHITE);
+                mulButton.setForeground(Color.WHITE);
+                subButton.setForeground(Color.WHITE);
+                addButton.setForeground(Color.WHITE);
+                equButton.setForeground(Color.WHITE);
+                negButton.setForeground(Color.WHITE);
+            }
+            case "Dark" -> {
+                final Color primaryDarkColor = new Color(229, 150, 150);
+                final Color secondaryDarkColor = new Color(154, 154, 154);
+                frame.getContentPane().setBackground(new Color(35, 35, 35));
+                numberButtons[0].setBackground(secondaryDarkColor);
+                numberButtons[1].setBackground(secondaryDarkColor);
+                numberButtons[2].setBackground(secondaryDarkColor);
+                numberButtons[3].setBackground(secondaryDarkColor);
+                numberButtons[4].setBackground(secondaryDarkColor);
+                numberButtons[5].setBackground(secondaryDarkColor);
+                numberButtons[6].setBackground(secondaryDarkColor);
+                numberButtons[7].setBackground(secondaryDarkColor);
+                numberButtons[8].setBackground(secondaryDarkColor);
+                numberButtons[9].setBackground(secondaryDarkColor);
+                decButton.setBackground(secondaryDarkColor);
+                clrButton.setForeground(secondaryDarkColor);
+                divButton.setForeground(secondaryDarkColor);
+                mulButton.setForeground(secondaryDarkColor);
+                subButton.setForeground(secondaryDarkColor);
+                addButton.setForeground(secondaryDarkColor);
+                equButton.setForeground(secondaryDarkColor);
+                clrButton.setBackground(primaryDarkColor);
+                divButton.setBackground(primaryDarkColor);
+                mulButton.setBackground(primaryDarkColor);
+                subButton.setBackground(primaryDarkColor);
+                addButton.setBackground(primaryDarkColor);
+                equButton.setBackground(primaryDarkColor);
+                delButton.setBackground(primaryDarkColor);
+                negButton.setBackground(primaryDarkColor);
+            }
+        }
+    };
 }
